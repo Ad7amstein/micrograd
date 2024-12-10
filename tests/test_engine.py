f8a13ad497bed2133afb7883fcf17315a342d139
Ad7amstein <adham32003200@gmail.com>
2024-12-10 22:16:21 +0200

    add more tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -28,10 +28,10 @@ class TestEngine(unittest.TestCase):
     def test_data(self):
         """To be filled"""
         with self.assertRaises(TypeError):
-            Value(data="5")
-            Value(data=[3])
-            Value(data=[3, 2])
-            Value(data={4.2})
+            _ = Value(data="5")
+            _ = Value(data=[3])
+            _ = Value(data=[3, 2])
+            _ = Value(data={4.2})
 
     def test_repr(self):
         """To be filled"""
@@ -71,3 +71,150 @@ class TestEngine(unittest.TestCase):
         val6 = 1 + val2
         self.assertIsInstance(val6, Value)
         self.assertAlmostEqual(val6.data, 6.9)
+
+    def test_mul(self):
+        """To be filled"""
+        val1 = Value(data=3.2)
+        val2 = Value(data=5.9)
+        val3 = val1 * val2
+        self.assertIsInstance(val3, Value)
+        self.assertAlmostEqual(val3.data, 18.88)
+
+        val4 = val1 * 5
+        self.assertIsInstance(val4, Value)
+        self.assertAlmostEqual(val4.data, 16)
+        val5 = val2 * 2.6
+        self.assertIsInstance(val5, Value)
+        self.assertAlmostEqual(val5.data, 15.34)
+
+        val1 *= 7
+        self.assertIsInstance(val1, Value)
+        self.assertAlmostEqual(val1.data, 22.4)
+        val6 = 1 * val2
+        self.assertIsInstance(val6, Value)
+        self.assertAlmostEqual(val6.data, 5.9)
+        val7 = val2 * 0
+        self.assertIsInstance(val7, Value)
+        self.assertAlmostEqual(val7.data, 0)
+        val8 = -1 * val1
+        self.assertIsInstance(val8, Value)
+        self.assertAlmostEqual(val8.data, -22.4)
+
+    def test_neg(self):
+        """To be filled"""
+        val = Value(data=10)
+        self.assertEqual(val.data, 10)
+        self.assertIsInstance(-val, Value)
+        val2 = -val
+        self.assertIsNot(val, val2)
+        self.assertEqual(val2.data, -10)
+
+    def test_sub(self):
+        """To be filled"""
+        val1 = Value(data=3.2)
+        val2 = Value(data=5.9)
+        val3 = val1 - val2
+        self.assertIsInstance(val3, Value)
+        self.assertAlmostEqual(val3.data, -2.7)
+        val3 = val2 - val1
+        self.assertIsInstance(val3, Value)
+        self.assertAlmostEqual(val3.data, 2.7)
+
+        val4 = val1 - 5
+        self.assertIsInstance(val4, Value)
+        self.assertAlmostEqual(val4.data, -1.8)
+        val5 = val2 - 2.6
+        self.assertIsInstance(val5, Value)
+        self.assertAlmostEqual(val5.data, 3.3)
+
+        val1 -= 7
+        self.assertIsInstance(val1, Value)
+        self.assertAlmostEqual(val1.data, -3.8)
+        val6 = 0 - val2
+        self.assertIsInstance(val6, Value)
+        self.assertAlmostEqual(val6.data, -5.9)
+        val7 = val2 - 0
+        self.assertIsInstance(val7, Value)
+        self.assertAlmostEqual(val7.data, 5.9)
+        val8 = -1 - val1
+        self.assertIsInstance(val8, Value)
+        self.assertAlmostEqual(val8.data, 2.8)
+
+    def test_pow(self):
+        """To be filled"""
+        val1 = Value(data=2)
+        val2 = Value(data=3.2)
+
+        self.assertIsInstance(val1**3, Value)
+        val3 = val1**3
+        self.assertEqual(val3.data, 8)
+        self.assertIsInstance(val1**-3, Value)
+        val3 = val1**-3
+        self.assertEqual(val3.data, 0.125)
+
+        self.assertIsInstance(val1**val2, Value)
+        val3 = val1**val2
+        self.assertAlmostEqual(val3.data, 9.1895868)
+        self.assertIsInstance(val2**val1, Value)
+        val3 = val2**val1
+        self.assertAlmostEqual(val3.data, 10.24)
+
+        val1 **= 1
+        self.assertIsInstance(val1, Value)
+        self.assertAlmostEqual(val1.data, 2)
+        val2 **= 0
+        self.assertIsInstance(val2, Value)
+        self.assertAlmostEqual(val2.data, 1)
+
+        val3 = 3**val1
+        self.assertIsInstance(val3, Value)
+        self.assertAlmostEqual(val3.data, 9)
+        val3 = 3**-val1
+        self.assertIsInstance(val3, Value)
+        self.assertAlmostEqual(val3.data, 0.11111111)
+        val3 = -(val1**-1)
+        self.assertIsInstance(val3, Value)
+        self.assertEqual(val3.data, -0.5)
+
+    def test_div(self):
+        """To be filled"""
+        val1 = Value(data=7)
+        val2 = Value(data=3.2)
+
+        self.assertIsInstance(val1 / 3, Value)
+        val3 = val1 / 3
+        self.assertAlmostEqual(val3.data, 2.33333333)
+        self.assertIsInstance(val1 / -3, Value)
+        val3 = val1 / -3
+        self.assertAlmostEqual(val3.data, -2.33333333)
+        self.assertIsInstance(-val1 / 3, Value)
+        val3 = -val1 / 3
+        self.assertAlmostEqual(val3.data, -2.33333333)
+
+        self.assertIsInstance(val1 / val2, Value)
+        val3 = val1 / val2
+        self.assertAlmostEqual(val3.data, 2.1875)
+        self.assertIsInstance(val2 / val1, Value)
+        val3 = val2 / val1
+        self.assertAlmostEqual(val3.data, 0.4571428571)
+
+        val1 /= 1
+        self.assertIsInstance(val1, Value)
+        self.assertAlmostEqual(val1.data, 7)
+        with self.assertRaises(ZeroDivisionError):
+            _ = val1 / 0
+            _ = 1 / Value(0)
+            val2 /= 0
+
+        val3 = 3 / val1
+        self.assertIsInstance(val3, Value)
+        self.assertAlmostEqual(val3.data, 0.4285714)
+        val3 = 0 / val1
+        self.assertIsInstance(val3, Value)
+        self.assertAlmostEqual(val3.data, 0)
+        val3 = 3 / -val1
+        self.assertIsInstance(val3, Value)
+        self.assertAlmostEqual(val3.data, -0.4285714)
+        val3 = -val1 / -1
+        self.assertIsInstance(val3, Value)
+        self.assertEqual(val3.data, 7)
